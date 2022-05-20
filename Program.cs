ea8ab80b9547cbc0d0eaa410e21f7199e3636064
Praful <prafulrakhade02@gmail.com>
2022-05-20 11:11:14 +0530

    [praful] Added : Simple Queues program file

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StackAndQueuesProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------WELCOME TO THE STACK AND QUEQUES-----------------------\n\n");
+            StackUsingLinkedlist obj = new StackUsingLinkedlist();
+
+            obj.Push(56);
+            obj.Push(30);
+            obj.Push(70);
+
+            obj.Display();
+
+            Console.WriteLine("\nPerforming Pop operation");
+            Console.WriteLine("Top peek item in stack is: {0}", obj.peek());
+            obj.pop();
+            Console.WriteLine("Top peek item in stack is: {0}", obj.peek());
+            obj.pop();
+            Console.WriteLine("Top peek item in stack is: {0}", obj.peek());
+            obj.pop();
+            obj.peek();
+
+
+
+
+            Console.WriteLine("Adding elements to Queue");
+            Queue q = new Queue();
+            q.enqueue(56);
+            q.enqueue(30);
+            q.enqueue(70);
+            Console.WriteLine("statrting element is: " + q.front.key);
+        }
+    }
+}
---
+++ QueueUsingLinkedlist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueuesProblem
+{
+    public class QNode
+    {
+
+        public int key;
+        public QNode next;
+
+        public QNode(int key)
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }
+    class Queue
+    {
+        QNode front, rear;
+
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+
+        public void enqueue(int key)
+        {
+            QNode temp = new QNode(key);
+
+            if (this.rear == null)
+            {
+                this.front = this.rear = temp;
+                return;
+            }
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+    }
+}
